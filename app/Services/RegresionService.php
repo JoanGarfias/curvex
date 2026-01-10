a6f72979b29d96ec645ae0f0831836b5382c3c6e
= <joanpagarf09@gmail.com>
2026-01-09 22:51:04 -0600

    refactor: regresion service

--- app/Services/RegresionService.php
+++ app/Services/RegresionService.php
@@ -102,35 +102,51 @@ class RegresionLineal extends RegresionData implements RegresionOperations {
             //limpieza del array de fila de la matriz de datos de las variables independientes
             $row_variable_value = [];
         }
-        
+
         return $sse;
     }
 
     public function calculateSST(): float {
         return array_reduce($this->dependent_data->points, fn($y) => $y, 0.0) / $this->y_avg;
-    } 
+    }
 
     public function calculateR2(): float {
         /*Paso 1: Calcular m, la cantidad de datos */
-        $m = (float) $this->countPoints();
+        $m = (float) $this->getM();
 
         /*Paso 2: Calcular las sumatorias (SSE, SSR, SST) */
-        $sum_y = array_reduce($this->points, fn(float $s, Point $p) => $s + $p->y, 0.0);
-        $sum_x = array_reduce($this->points, fn(float $s, Point $p) => $s + $p->x, 0.0);
-        $sum_x2 = array_reduce($this->points, fn(float $s, Point $p) => $s + pow($p->x, 2), 0.0);
+
+        /** @var float[] */
+        $sum_value_variables = [];
+        /** @var float[] */
+        $sum_value_variables_squared = [];
+        $sum_y = array_reduce($this->dependent_data->points, fn(float $s, float $y) => $s + $y);
+
+        foreach($this->data as $variable_data){
+            $sum_value_variables = array_reduce($variable_data->points, fn(float $s, float $value) => $s + $value, 0.0);
+            $sum_value_variables_squared = array_reduce($variable_data->points, fn(float $s, float $value) => $s + pow($value, 2), 0.0);
+        }
+
+
+        Log::info("Sumatoria de valores de las variables ", $sum_value_variables, " sumatoria de valores de las variables al cuadrado", $sum_value_variables_squared);
+        /*
         $sum_xy = array_reduce($this->points, fn(float $s, Point $p) => $s + ($p->x * $p->y), 0.0);
         Log::info("Sumatorias calculadas: sum_y = $sum_y, sum_x = $sum_x, sum_x2 = $sum_x2, sum_xy = $sum_xy");
+        */
 
         try{
             $this->y_avg = $sum_y / $m;
 
+            /*
             $matriz = new Matrix(
                 [
                     [$m, $sum_x, $sum_y],
                     [$sum_x, $sum_x2, $sum_xy],
                 ], 2, 3);
 
-            $this->a = CrammerSolver::solveCrammerMatrix2X2($matriz);
+            */
+            //Implementar lógica para obtener las soluciones
+            //$this->a = CrammerSolver::solveCrammerMatrix2X2($matriz);
 
             Log::info("Coeficientes calculados: a0 = {$this->a->a0}, a1 = {$this->a->a1}");
 
@@ -154,84 +170,16 @@ class RegresionLineal extends RegresionData implements RegresionOperations {
 }
 
 
-class Regresion3V implements RegresionOperations {
+class RegresionExponencial implements RegresionOperations {
     /** @var Point[] */
     private array $points = [];
     private float $y_avg = 0.0;
     private float $SSE = 0.0;
     private float $SST = 0.0;
-    private ?Solution2VSystem $a = null;
     private string $method = "lineal";
-
-    public function __construct(array $points, string $method = "lineal")
-    {
-        $this->points = $points;
-        $this->method = $method;
-    }
-
-    private function countPoints(): int
-    {
-        return count($this->points);
-    }
-
-    public function calculateSSE(): float {
-        $sse = 0.0;
-
-        foreach($this->points as $p){
-            //y gorrito
-            $y_pri = $this->a->a0 + $this->a->a1 * $p->x;
-            $sse += pow($y_pri-$p->y, 2);
-        }
-        
-        return $sse;
-    }
-
-    public function calculateSST(): float {
-        $sst = 0.0;
-
-        foreach($this->points as $p){
-            $sst += pow($p->y - $this->y_avg,   2);
-        }
-        
-        return $sst;
-    } 
-
-    public function calculateR2(): float {
-        /*Paso 1: Calcular m, la cantidad de datos */
-        $m = (float) $this->countPoints();
-
-        /*Paso 2: Calcular las sumatorias (SSE, SSR, SST) */
-        $sum_y = array_reduce($this->points, fn(float $s, Point $p) => $s + $p->y, 0.0);
-        $sum_x = array_reduce($this->points, fn(float $s, Point $p) => $s + $p->x, 0.0);
-        $sum_x2 = array_reduce($this->points, fn(float $s, Point $p) => $s + pow($p->x, 2), 0.0);
-        $sum_xy = array_reduce($this->points, fn(float $s, Point $p) => $s + ($p->x * $p->y), 0.0);
-        Log::info("Sumatorias calculadas: sum_y = $sum_y, sum_x = $sum_x, sum_x2 = $sum_x2, sum_xy = $sum_xy");
-
-        try{
-            $this->y_avg = $sum_y / $m;
-
-            $matriz = new Matrix(
-                [
-                    [$m, $sum_x, $sum_y],
-                    [$sum_x, $sum_x2, $sum_xy],
-                ], 2, 3);
-
-            $this->a = CrammerSolver::solveCrammerMatrix2X2($matriz);
-
-            Log::info("Coeficientes calculados: a0 = {$this->a->a0}, a1 = {$this->a->a1}");
-
-            $this->SSE = $this->calculateSSE();
-            $this->SST = $this->calculateSST();
-
-            Log::info("SSE = $this->SSE, SST = $this->SST");
-
-            $R2 = 1 - ($this->SSE / $this->SST);
-            return $R2;
-        } catch (Exception $e) {
-            Log::error("Error al calcular los coeficientes de regresión: " . $e->getMessage());
-            throw $e;
-        }
-    }
+    public function calculateSSE(){}
+    public function calculateSST(){}
+    public function calculateR2(){}
 }
 
 class RegresionService
@@ -239,8 +187,8 @@ class RegresionService
     public static function createRegresion(array $points, int $variables = 2, string $method = "lineal"){
         return
             match($variables){
-                1 => new Regresion1V($points, $method),
-                3 => new Regresion3V($points, $method),
+                1 => new RegresionLineal($points, $points, $method),
+                3 => new RegresionExponencial($points, $method),
                 default => throw new Exception("No existe una solución de regresión disponible para la información proporcionada.")
             }
         ;
